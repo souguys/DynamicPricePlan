d954c4af1026dc8aa6c28e286f65101e0341b5f7
Vimalesh <kmvimalesh@gmail.com>
2015-09-30 10:08:33 -0700

    Committing competitor price render

---
+++ src/com/verizon/dynamicpp/data/CompetitorPricePlanObject.java
@@ -0,0 +1,34 @@
+package com.verizon.dynamicpp.data;
+
+public class CompetitorPricePlanObject {
+	String plansize;
+	String att;
+	String sprint;
+	String tmobile;
+	public String getPlansize() {
+		return plansize;
+	}
+	public void setPlansize(String plansize) {
+		this.plansize = plansize;
+	}
+	public String getAtt() {
+		return att;
+	}
+	public void setAtt(String att) {
+		this.att = att;
+	}
+	public String getSprint() {
+		return sprint;
+	}
+	public void setSprint(String sprint) {
+		this.sprint = sprint;
+	}
+	public String getTmobile() {
+		return tmobile;
+	}
+	public void setTmobile(String tmobile) {
+		this.tmobile = tmobile;
+	}
+
+	
+}
--- src/com/verizon/dynamicpp/data/ConvertObjToJson.java
+++ src/com/verizon/dynamicpp/data/ConvertObjToJson.java
@@ -10,48 +10,105 @@ import java.util.Random;
 import com.google.gson.Gson;
 
 public class ConvertObjToJson {
-public String getVerizonCurrentData() {
-	Gson gson = new Gson();
-	Properties prop = new Properties();
-	InputStream input = null;
-	List<PricePlanObject> list = new ArrayList<PricePlanObject>();
-	try {
-
-		//input = new FileInputStream("verizonpricedata.properties");
-		input = this.getClass().getClassLoader().getResourceAsStream("com/verizon/dynamicpp/properties/verizonpricedata.properties");
-
-// load a properties file
-prop.load(input);
-
-// get the property value and print it out
-System.out.println(prop.getProperty("verizonplan"));
-System.out.println(prop.getProperty("verizonprice"));
-String verizonPlan = prop.getProperty("verizonplan");
-String verizonPrice = prop.getProperty("verizonprice");
-String [] vzPlanArr = verizonPlan.split(",");
-String [] vzPriceArr = verizonPrice.split(",");
-System.out.println(vzPlanArr.length);
-
-PricePlanObject pricePlanObj = null;
-for(int i=0;i<vzPlanArr.length;i++){
-	pricePlanObj = new PricePlanObject();
-	pricePlanObj.setData(vzPriceArr[i]);
-	pricePlanObj.setName(vzPlanArr[i]);
-	list.add(pricePlanObj);
-}
-System.out.println("data:{"+gson.toJson(list)+"}");
-	} catch (IOException ex) {
-		ex.printStackTrace();
-	} finally {
-		if (input != null) {
-			try {
-				input.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+	public String getVerizonCurrentData() {
+		Gson gson = new Gson();
+		Properties prop = new Properties();
+		InputStream input = null;
+		List<PricePlanObject> list = new ArrayList<PricePlanObject>();
+		try {
+			
+			//input = new FileInputStream("verizonpricedata.properties");
+			input = this.getClass().getClassLoader().getResourceAsStream("com/verizon/dynamicpp/properties/verizonpricedata.properties");
+			
+			// load a properties file
+			prop.load(input);
+			
+			// get the property value and print it out
+			System.out.println(prop.getProperty("verizonplan"));
+			System.out.println(prop.getProperty("verizonprice"));
+			String verizonPlan = prop.getProperty("verizonplan");
+			String verizonPrice = prop.getProperty("verizonprice");
+			String [] vzPlanArr = verizonPlan.split(",");
+			String [] vzPriceArr = verizonPrice.split(",");
+			System.out.println(vzPlanArr.length);
+			
+			PricePlanObject pricePlanObj = null;
+			for(int i=0;i<vzPlanArr.length;i++){
+				pricePlanObj = new PricePlanObject();
+				pricePlanObj.setData(vzPriceArr[i]);
+				pricePlanObj.setName(vzPlanArr[i]);
+				list.add(pricePlanObj);
+			}
+			System.out.println("data:{"+gson.toJson(list)+"}");
+		} 
+		catch (IOException ex) {
+			ex.printStackTrace();
+		} 
+		finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
+		return "data:{"+gson.toJson(list)+"}";
 	}
-	return "data:{"+gson.toJson(list)+"}";
-}
+	
+	public String getCompetitorData() {
+		Gson gson = new Gson();
+		Properties prop = new Properties();
+		InputStream input = null;
+		List<CompetitorPricePlanObject> list = new ArrayList<CompetitorPricePlanObject>();
+		try {
+			
+			//input = new FileInputStream("verizonpricedata.properties");
+			input = this.getClass().getClassLoader().getResourceAsStream("com/verizon/dynamicpp/properties/competitorprice.properties");
+			
+			// load a properties file
+			prop.load(input);
+			
+			// get the property value and print it out
+			System.out.println(prop.getProperty("competitorplan"));
+			System.out.println(prop.getProperty("attprice"));
+			System.out.println(prop.getProperty("sprintprice"));
+			System.out.println(prop.getProperty("tmobileprice"));
+			String competitorPlan = prop.getProperty("competitorplan");
+			String attPrice = prop.getProperty("attprice");
+			String sprintPrice = prop.getProperty("sprintprice");
+			String tmobilePrice = prop.getProperty("tmobileprice");
+			String [] competitorPlanArr = competitorPlan.split(",");
+			String [] attPriceArr = attPrice.split(",");
+			String [] sprintPriceArr = sprintPrice.split(",");
+			String [] tmobilePriceArr = tmobilePrice.split(",");
+			System.out.println(competitorPlanArr.length);
+			
+			CompetitorPricePlanObject pricePlanObj = null;
+			for(int i=0;i<competitorPlanArr.length;i++){
+				pricePlanObj = new CompetitorPricePlanObject();
+				pricePlanObj.setPlansize(competitorPlanArr[i]);
+				pricePlanObj.setAtt(attPriceArr[i]);
+				pricePlanObj.setSprint(sprintPriceArr[i]);
+				pricePlanObj.setTmobile(tmobilePriceArr[i]);
+				list.add(pricePlanObj);
+			}
+			System.out.println("data:{"+gson.toJson(list)+"}");
+		} 
+		catch (IOException ex) {
+			ex.printStackTrace();
+		} 
+		finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return "data:{"+gson.toJson(list)+"}";
+	}
+
 
 }
