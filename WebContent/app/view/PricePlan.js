1645ff59de7199ec016dbf8ba1dbd382400b11cb
Vimalesh <kmvimalesh@gmail.com>
2015-09-30 02:02:48 -0700

    Dummy file

--- WebContent/app/view/PricePlan.js
+++ WebContent/app/view/PricePlan.js
@@ -1,28 +1,25 @@
 Ext.define("Verizon.view.PricePlan",{
-	extend:"Ext.panel.Panel",
-	xtype:"priceplan",
-	title:"Price Plan Automation",
-	height:500,width:500,
-	pack:'center',
+	extend : "Ext.panel.Panel",
+	layout: "border",
+	xtype : "priceplan",
+	title : "Welcome home",
 	items:[
-	       {
-	    	   xtype : "textfield",
-	    	   fieldLabel:"UserName",
-	    	   id:"name"
-	    		   
-	       },
-	       {
-	    	   xtype : "textfield",
-	    	   fieldLabel:"Password",
-	    	   id:"pass"
-	       },
-	       {
-	    	   xtype : "button",
-	    	   text:"Login",
-	    	   id:"login"
-	       }
-	       
-	       ]
+	       		{
+	       			xtype : "northpanel",
+	       			region : "north",
+	       			id : "northhomepanel"
+	       		},
+	       		{
+	       			xtype : "centerhomepanel",
+	       			region : "center",
+	       			id : "centerhomepanel"
+				},
+				{
+					xtype : "westhomepanel",
+					region : "west",
+						id : "westhomepanel"
+				}
 
-	
+
+	       ]
 });
