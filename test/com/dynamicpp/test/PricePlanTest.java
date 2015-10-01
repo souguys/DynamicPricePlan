e1300f2fda0c91be2b2db8551672ed67aef3397e
Vimalesh <kmvimalesh@gmail.com>
2015-10-01 01:57:16 -0700

    Make build success

--- test/com/dynamicpp/test/PricePlanTest.java
+++ test/com/dynamicpp/test/PricePlanTest.java
@@ -12,7 +12,7 @@ public class PricePlanTest {
 	@Test
 	public void test() {
 		ConvertObjToJson testAvg = new ConvertObjToJson();
-		assertEquals("Unit Test Case Pass", 15, testAvg.getAvg(10, 10, 10));
+		assertEquals("Unit Test Case Pass", 10, testAvg.getAvg(10, 10, 10));
 	}
 
 }
