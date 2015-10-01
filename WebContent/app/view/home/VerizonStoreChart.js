a73c4b4e72f0afffcf5191534bb71e8bde1c4fa9
JKarthik <Administrator@scsbwin-930155.ITTPA.EBIZ.VERIZON.COM>
2015-10-01 04:20:17 -0700

    Commiting Pie chart

--- WebContent/app/view/home/VerizonStoreChart.js
+++ WebContent/app/view/home/VerizonStoreChart.js
@@ -1,53 +1,44 @@
 Ext.define('Verizon.view.home.VerizonStoreChart', {
 	extend: 'Ext.chart.Chart',
 	xtype: "verizonstorechart",
-    renderTo: Ext.getBody(),
     width: 500,
     height: 225,
     animate: true,
     store: "VerizonStore",
-    axes: [
-        {
-            type: 'Numeric',
-            position: 'left',
-            fields: ['data'],
-            label: {
-                renderer: Ext.util.Format.numberRenderer('0,0')
-            },
-            title: 'Price in $',
-            grid: true,
-            minimum: 0
+    animate: true,
+    shadow: true,
+    legend: {
+        position: 'right'
+    },
+    insetPadding: 60,
+    theme: 'Base:gradients',
+    series: [{
+        type: 'pie',
+        field: 'data',
+        showInLegend: true,
+        //donut: donut,
+        tips: {
+            trackMouse: true,
+            renderer: function(storeItem, item) {
+                //calculate percentage.
+                var total = 0;
+              /*  store1.each(function(rec) {
+                    total += rec.get('data');
+                });*/
+               // this.setTitle(storeItem.get('name') + ': ' + Math.round(storeItem.get('data') / total * 100) + '%');*/
+                this.setTitle(storeItem.get('data'));
+            }
         },
-        {
-            type: 'Category',
-            position: 'bottom',
-            fields: ['name'],
-            title: 'Plan Size'
+        highlight: {
+            segment: {
+                margin: 20
+            }
+        },
+        label: {
+            field: 'name',
+            display: 'rotate',
+            contrast: true,
+            font: '18px Arial'
         }
-    ],
-    series: [
-        {
-            type: 'column',
-            axis: 'left',
-            highlight: true,
-            tips: {
-              trackMouse: true,
-              width: 140,
-              height: 28,
-              renderer: function(storeItem, item) {
-                this.setTitle(storeItem.get('name') + ': ' + storeItem.get('data') + ' $');
-              }
-            },
-            label: {
-              display: 'insideEnd',
-              'text-anchor': 'middle',
-                field: 'data',
-                renderer: Ext.util.Format.numberRenderer('0'),
-                orientation: 'vertical',
-                color: '#333'
-            },
-            xField: 'name',
-            yField: 'data'
-        }
-    ]
+    }]
 });
